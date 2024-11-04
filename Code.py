d899c7fe432ca3e8e968bd12e2e15b02db973620
RPrincy <rathi.princy93@gmail.com>
2024-11-04 15:37:54 -0600

    mini-project |  Linear Regression model

--- Code.py
+++ Code.py
@@ -2,10 +2,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_percentage_error
 
 
 #importing the dataset
-data = pd.read_csv("c:/Users/bharg/Downloads/MSITM_Python/Professor_Git_Hub/NVIDIA-Stock-Analysis-Python/nvidia_stock_data.csv")
+data = pd.read_csv("D:/Python/nvidia_stock_data.csv")
 
 #converting the dataset into a dataframe
 df = pd.DataFrame(data)
@@ -27,3 +29,26 @@ plt.title("Correlation Matrix")
 plt.savefig('correlation_matrix.jpg')
 plt.show()
 df_2 = df_1.drop(columns='Adj Close') #2. preprocessing step (Removing Adj Close to avoid multicollinearity)
+
+#building a Linear Regression model (#1.machine learning model) to predict the close values with a train data of 80% and a test data of 20%
+X = df_2[['Open', 'High', 'Low', 'Volume']] #independent variables
+y = df_2['Close']#dependent variables
+#splitting the dataset
+X_train = X.iloc[:350]
+y_train = y.iloc[:350]
+X_test = X.iloc[350:437]
+y_test = y.iloc[350:437]
+#creating a model and training it
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+#making predictions based on the test set
+y_linear_predictions = model.predict(X_test)
+
+#model evaluation metrics for the Linear Regression Model
+r2_linear = r2_score(y_test,y_linear_predictions)
+mse_linear = mean_squared_error(y_test, y_linear_predictions)
+mape_linear = mean_absolute_percentage_error(y_test, y_linear_predictions)
+print(f"Mean Squared Error of the linear Regression model: {mse_linear:.4f}%")
+print(f"R-squared of the linear Regression model: {r2_linear:.4f}%")
+print(f"Mean Absolute Percentage Error of the linear Regression model: {mape_linear * 100:.2f}%")
